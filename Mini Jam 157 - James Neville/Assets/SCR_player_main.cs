3aee06cf3711fc94467e1a45034d962bbf58617a
Izzet <jamesliamneville@gmail.com>
2024-04-28 12:41:28 +0100

    Basic Movement & Jumping Complete

--- Mini Jam 157 - James Neville/Assets/SCR_player_main.cs
+++ Mini Jam 157 - James Neville/Assets/SCR_player_main.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SCR_player_main : MonoBehaviour {
-    [SerializeField] private Transform centerOfMass;
 
     [Header("Movement")]
     [SerializeField] private float speed;
+    [SerializeField] private float counterCorrectingSpeed;
     [SerializeField] private float velocityMax;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpForceX;
 
     [Header("Other")]
     [SerializeField] private LayerMask ground;
+    [SerializeField] private float groundedSpriteRotationSpeed;
+    [SerializeField] private float airborneSpriteRotationSpeed;
 
     [Header("Components (Read Only)")]
     [SerializeField] [MyReadOnly] private Rigidbody2D rb;
@@ -21,7 +24,7 @@ public class SCR_player_main : MonoBehaviour {
 
     [Header("Info (Read Only)")]
     [SerializeField] [MyReadOnly] private float inputX;
-    [SerializeField] [MyReadOnly] private bool canJump;
+    [SerializeField] [MyReadOnly] private bool grounded;
 
     public static SCR_player_main instance { get; private set; }
 
@@ -32,41 +35,59 @@ public class SCR_player_main : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponentInChildren<SpriteRenderer>();
         col = GetComponentInChildren<Collider2D>();
-
-        rb.centerOfMass = centerOfMass.position;
     }
     private void Update() {
         playerWalkCalculations();
         jumpCheck();
+        spriteRotation();
     }
     private void FixedUpdate() {
         rb.AddForce(new Vector2(inputX, 0) * speed, ForceMode2D.Force);
     }
 
     private void playerWalkCalculations() {
-        inputX = Input.GetAxis("Horizontal");
+        if(inputX > 0 && rb.velocity.x < 0 || inputX < 0 && rb.velocity.x > 0) { //If player is trying to go in opposite direction of force
+            inputX = Input.GetAxisRaw("Horizontal") * counterCorrectingSpeed;
+            print(inputX);
+        }
+        else {
+            inputX = Input.GetAxisRaw("Horizontal");
+        }
 
-        Vector3 unclampedSlerp = Vector3.Slerp(sr.gameObject.transform.up, -rb.velocity.normalized, Time.deltaTime);
-        Vector3 clampedSlerp = new Vector3(Mathf.Clamp(unclampedSlerp.x, -.3f, .3f), Mathf.Clamp(unclampedSlerp.y, -3, 3));
-
-        sr.gameObject.transform.up = clampedSlerp; //Quaternion.Euler(0, 0, Mathf.Clamp(rb.velocity.x * 10, -8, 8));
-
-        //rb.velocity = new Vector2(Vector2.ClampMagnitude(rb.velocity, velocityMax).x, rb.velocity.y);
+        //if (canJump) {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -velocityMax, velocityMax), rb.velocity.y);
+        //}
     }
-    private void jumpCheck() {
+    private void jumpCheck() {        
+        RaycastHit2D groundedBoxCast = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0, Vector2.down, .1f, ground);
+        if (groundedBoxCast.collider != null && Mathf.Abs(rb.velocity.y) <= .01f) {
+            grounded = true;
+        }
+        else {
+            grounded = false;
+        }
+
         if (Input.GetKey(KeyCode.Space)) {
-            if (canJump) {
+            if (grounded) {
                 jump();
             }
         }
-        
-        RaycastHit2D groundedBoxCast = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0, Vector2.down, .1f, ground);
-        if (groundedBoxCast.collider != null) {
-            canJump = true;
-        }
     }
     private void jump() {
-        rb.AddForce(new Vector2(0, 1) * jumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        canJump = false;
+        rb.AddForce(new Vector2(0, 1) * jumpForce, ForceMode2D.Impulse);
+
+        if(Mathf.Abs(rb.velocity.x) > 0.3f) {
+            float dir;
+            dir = rb.velocity.normalized.x;
+            rb.AddForce(new Vector2(dir, 0) * jumpForceX * Mathf.Clamp(Mathf.Abs(rb.velocity.x), 0, 2), ForceMode2D.Impulse);
+        }
+    }
+    private void spriteRotation() {
+        float rotationSpeed = (grounded) ? groundedSpriteRotationSpeed : airborneSpriteRotationSpeed;
+        Quaternion unclampedSlerp = Quaternion.Lerp(sr.gameObject.transform.rotation, Quaternion.Euler(0f, 0f, Mathf.Clamp(inputX, -1, 1) * 30), rotationSpeed * Time.deltaTime);
+
+        sr.gameObject.transform.rotation = unclampedSlerp;
+        
+        //sr.gameObject.transform.rotation = Quaternion.Lerp(sr.gameObject.transform.rotation, Quaternion.Euler(0f, 0f, 0f), airborneSpriteRotationSpeed * Time.deltaTime);
     }
 }
